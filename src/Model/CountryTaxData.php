6789f9fa85e8cc8f450bfd58499fe21eebdd4f16
yamaha252 <mail@yamaha252.ru>
2019-07-29 16:12:35 +0200

    restore missed docs

--- src/Model/CountryTaxData.php
+++ src/Model/CountryTaxData.php
@@ -4,7 +4,18 @@ namespace Model;
 
 class CountryTaxData
 {
+    /**
+     * @var int
+     */
     public $countryId;
+
+    /**
+     * @var float Average tax rate of the country
+     */
     public $averageTaxRate;
+
+    /**
+     * @var float Overall amount of country taxes
+     */
     public $overallTaxAmount;
 }
--- src/Model/StateTaxData.php
+++ src/Model/StateTaxData.php
@@ -4,8 +4,23 @@ namespace Model;
 
 class StateTaxData
 {
+    /**
+     * @var int
+     */
     public $stateId;
+
+    /**
+     * @var float Overall amount of state taxes
+     */
     public $overallTaxAmount;
+
+    /**
+     * @var float Average amount of state taxes
+     */
     public $averageTaxAmount;
+
+    /**
+     * @var float Average tax rate of state
+     */
     public $averageTaxRate;
 }
